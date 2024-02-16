9df0e6bf901b69bf2cd3ce4ecc55c818244af8a7
Davlatjon Shavkatov <dalisoft.dev@gmail.com>
2024-02-17 04:02:52 +0500

    feat(plugins): move `git` plugin

---
+++ plugins/git.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+release() {
+  # Create a `git` tag
+  echo "Creating Git tag..."
+  if ! $IS_DRY_RUN; then
+    git tag "$RELEASE_TAG_NAME" "$CHECKOUT_SHA"
+    git push origin "refs/tags/$RELEASE_TAG_NAME"
+    echo "Created Git tag [$RELEASE_TAG_NAME]!"
+  else
+    echo "Skipped Git tag [$RELEASE_TAG_NAME] in DRY-RUN mode."
+  fi
+}
