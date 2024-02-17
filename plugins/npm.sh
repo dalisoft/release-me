c37d280fb522842c76b6810200bd2a416f12187c
Davlatjon Shavkatov <dalisoft.dev@gmail.com>
2024-02-18 04:32:49 +0500

    fix(plugins): `npm` plugin now should work properly

--- plugins/npm.sh
+++ plugins/npm.sh
@@ -3,13 +3,23 @@ set -e
 
 release() {
   # Publish a `npm` tag
-  log "Publishing npm tag..."
-  log_verbose "npm tag: $RELEASE_TAG_NAME!"
+  if [[ "$NPM_TOKEN" != "" ]]; then
+    rm -rf .npmrc
+    echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >>.npmrc
+    log "Publishing npm tag..."
+    log_verbose "npm tag: $RELEASE_TAG_NAME!"
 
-  if ! $IS_DRY_RUN; then
-    npm publish "$RELEASE_TAG_NAME"
-    echo "Published [$RELEASE_TAG_NAME]!"
+    if ! $IS_DRY_RUN; then
+      npm publish "$RELEASE_TAG_NAME"
+      echo "Published [$RELEASE_TAG_NAME]!"
+      rm -rf .npmrc
+    else
+      log "Skipped npm tag [$RELEASE_TAG_NAME] in DRY-RUN mode."
+    fi
   else
-    log "Skipped npm tag [$RELEASE_TAG_NAME] in DRY-RUN mode."
+    echo "
+npm Token is not found
+Please export npm Token so this plugin can be used
+"
   fi
 }
