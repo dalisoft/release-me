7b11d79236a72347878cf0e08ac766a7a346f904
Davlatjon Shavkatov <dalisoft.dev@gmail.com>
2024-02-21 04:01:13 +0500

    fix: `git` commit counter fix for empty workspace

--- release.sh
+++ release.sh
@@ -263,7 +263,7 @@ function getGitCommits {
       GIT_LOGS_LENGTH=$(git log HEAD --grep "$PKG_NAME" --pretty=format:"%s" | glc -)
     else
       GIT_LOGS=$(git log HEAD --pretty=format:"$GIT_LOG_FORMAT" --reverse)
-      GIT_LOGS_LENGTH=$(git rev-list --count "$GIT_LAST_PROJECT_TAG...HEAD")
+      GIT_LOGS_LENGTH=$(git rev-list --count)
     fi
   fi
 
