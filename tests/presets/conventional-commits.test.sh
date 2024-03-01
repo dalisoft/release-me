73c1efb8739243755a3ae4ca8f08683f37993f04
Davlatjon Shavkatov <opensource@dalisoft.uz>
2024-03-01 08:55:19 +0500

    chore(tests): add gpg fake keys

--- tests/presets/conventional-commits.test.sh
+++ tests/presets/conventional-commits.test.sh
@@ -40,10 +40,33 @@ teardown_suite() {
 ## https://conventionalcommits.org ##
 #####################################
 
+test_commit_2_gpg_key() {
+  export GPG_PASSPHRASE="123456890"
+  export GNUPGHOME="$(mktemp -d)"
+  cat >fakegpg <<EOF
+     %echo Generating a basic OpenPGP key
+     Key-Type: RSA
+     Key-Length: 4096
+     Subkey-Type: ELG-E
+     Subkey-Length: 4096
+     Name-Real: $GIT_USERNAME
+     Name-Comment: with stupid passphrase
+     Name-Email: $GIT_EMAIL
+     Expire-Date: 0
+     Passphrase: $GPG_PASSPHRASE
+     # Do a commit here, so that we can later print "done" :-)
+     %commit
+     %echo done
+EOF
+  gpg --batch --generate-key fakegpg
+  export GPG_KEY_ID=$(gpg -k opensource@dalisoft.uz | head -2 | tail -1 | xargs)
+  export GPG_KEY=$(gpg --quiet --passphrase "$GPG_PASSPHRASE" --batch --pinentry-mode loopback --export-secret-keys "$GPG_KEY_ID" | base64)
+}
+
 test_commit_1_feat_breaking_major_message() {
   git commit -m "feat: allow provided config object to extend other configs" -m "BREAKING CHANGE: \`extends\` key in config file is now used for extending other config files" --allow-empty --no-gpg-sign
-  bash "$ROOT_DIR/release.sh" --plugins=git --quiet --stable
 
+  bash "$ROOT_DIR/release.sh" --plugins=git --quiet --stable
   assert_equals "v1.0.0" "$(git tag -l | tail -1)"
 }
 test_commit_2_feat_mark_major_message() {
@@ -51,6 +74,8 @@ test_commit_2_feat_mark_major_message() {
 
   bash "$ROOT_DIR/release.sh" --plugins=git --quiet --dry-run
   assert_equals "v1.0.0" "$(git tag -l | tail -1)"
+
+  assert_matches "v2.0.0" "$(bash "$ROOT_DIR/release.sh" --plugins=git --verbose --dry-run)"
 
   bash "$ROOT_DIR/release.sh" --plugins=git --quiet
   assert_equals "v2.0.0" "$(git tag -l | tail -1)"
