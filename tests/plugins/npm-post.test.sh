762bd9b505cf0e9502dbe9baf57c3fc3427a5b3f
Davlatjon Shavkatov <opensource@dalisoft.uz>
2024-04-25 15:05:01 +0500

    tests(npm-post): add new test-case

---
+++ tests/plugins/npm-post.test.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+set -eu
+
+ROOT_DIR="$(realpath ../../)"
+REPO_FOLDER=$(mktemp -d)
+
+setup_suite() {
+  cd "$REPO_FOLDER"
+  git init --quiet --initial-branch=master
+
+  echo '{
+  "name": "fake-me",
+  "version": "1.0.0",
+  "description": "> Except bugs, errors and/or strange behavior",
+  "main": "index.js",
+  "directories": {
+    "doc": "docs",
+    "test": "tests"
+  },
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC"
+}
+' >>"$REPO_FOLDER/package.json"
+
+  export GIT_DIR="$REPO_FOLDER/.git"
+  export GIT_CONFIG="$GIT_DIR/.gitconfig"
+  export GIT_WORK_TREE="$REPO_FOLDER"
+
+  _npm() {
+    # shellcheck disable=SC2317
+    if [[ "${FAKE_PARAMS[0]}" == "publish" && "${NPM_TOKEN-}" == "FAKE_TOKEN" ]]; then
+      return 0
+    else
+      exit 1
+    fi
+  }
+  export -f _npm
+
+  fake npm _npm
+}
+
+teardown_suite() {
+  rm -rf "$GIT_WORK_TREE"
+  unset REPO_FOLDER
+  unset GIT_DIR
+  unset GIT_CONFIG
+  unset GIT_WORK_TREE
+}
+
+#####################################
+## This tests of specification at  ##
+## https://conventionalcommits.org ##
+#####################################
+
+test_plugin_npm_0_1_initial_message_dryrun() {
+  git commit --quiet -m "fix: initial commit" --allow-empty
+
+  NPM_TOKEN="FAKE_TOKEN" bash "$ROOT_DIR/release.sh" --plugins=npm,npm-post --quiet --dry-run
+  assert_matches "1.0.0" "$(cat package.json)"
+}
+test_plugin_npm_0_2_initial_message() {
+  assert_matches "v0.0.1" "$(NPM_TOKEN="FAKE_TOKEN" bash "$ROOT_DIR/release.sh" --plugins=npm,npm-post --quiet)"
+  assert_matches "0.0.1" "$(cat package.json)"
+}
+test_plugin_npm_no_token_fail_message() {
+  assert_status_code 1 "$ROOT_DIR/release.sh --plugins=npm,npm-post --quiet"
+}
