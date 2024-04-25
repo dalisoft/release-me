3791254fce3c634f1f3d30beb1b9c8c3a5a33cf9
Davlatjon Shavkatov <opensource@dalisoft.uz>
2024-04-25 19:41:10 +0500

    test: update test-cases

--- tests/presets/conventional-commits.test.sh
+++ tests/presets/conventional-commits.test.sh
@@ -60,74 +60,80 @@ test_commit_0_3_skip_change() {
   bash "$ROOT_DIR/release.sh" --plugins=git --preset=conventional-commits --quiet
   assert_equals "v0.0.1" "$(git tag -l | tail -1)"
 }
-test_commit_1_feat_breaking_major_message() {
-  git commit --quiet -m "feat: allow provided config object to extend other configs" -m "BREAKING CHANGE: \`extends\` key in config file is now used for extending other config files" --allow-empty
+test_commit_1_0_stable_major_no_message() {
+  git commit --quiet -m "fix: patch update" --allow-empty
 
   bash "$ROOT_DIR/release.sh" --plugins=git --quiet --stable
   assert_matches "v1.0.0" "$(git tag -l)"
+}
+test_commit_1_1_feat_breaking_major_message() {
+  git commit --quiet -m "feat: allow provided config object to extend other configs" -m "BREAKING CHANGE: \`extends\` key in config file is now used for extending other config files" --allow-empty
+
+  bash "$ROOT_DIR/release.sh" --plugins=git --quiet
+  assert_matches "v2.0.0" "$(git tag -l)"
 }
 test_commit_2_feat_mark_major_message() {
   git commit --quiet -m "feat!: send an email to the customer when a product is shipped" --allow-empty
 
   bash "$ROOT_DIR/release.sh" --plugins=git --quiet --dry-run
-  assert_matches "v1.0.0" "$(git tag -l)"
+  assert_matches "v2.0.0" "$(git tag -l)"
 
-  assert_matches "v2.0.0" "$(bash "$ROOT_DIR/release.sh" --plugins=git --dry-run)"
+  assert_matches "v3.0.0" "$(bash "$ROOT_DIR/release.sh" --plugins=git --dry-run)"
 
   bash "$ROOT_DIR/release.sh" --plugins=git --quiet
-  assert_matches "v2.0.0" "$(git tag -l)"
+  assert_matches "v3.0.0" "$(git tag -l)"
 }
 test_commit_3_feat_mark_scope_major_message() {
   git commit --quiet -m "feat(api)!: send an email to the customer when a product is shipped" --allow-empty
 
   bash "$ROOT_DIR/release.sh" --plugins=git --quiet --dry-run
-  assert_matches "v2.0.0" "$(git tag -l)"
+  assert_matches "v3.0.0" "$(git tag -l)"
 
   bash "$ROOT_DIR/release.sh" --plugins=git --quiet
-  assert_matches "v3.0.0" "$(git tag -l)"
+  assert_matches "v4.0.0" "$(git tag -l)"
 }
 test_commit_4_feat_mark_breaking_scope_major_message() {
   git commit --quiet -m "chore!: drop support for Node 6" -m "BREAKING CHANGE: use JavaScript features not available in Node 6." --allow-empty
 
   bash "$ROOT_DIR/release.sh" --plugins=git --quiet --dry-run
-  assert_matches "v3.0.0" "$(git tag -l)"
+  assert_matches "v4.0.0" "$(git tag -l)"
 
   bash "$ROOT_DIR/release.sh" --plugins=git --quiet
-  assert_matches "v4.0.0" "$(git tag -l)"
+  assert_matches "v5.0.0" "$(git tag -l)"
 }
 test_commit_5_docs_no_update_message() {
   git commit --quiet -m "docs: correct spelling of CHANGELOG" --allow-empty
 
   bash "$ROOT_DIR/release.sh" --plugins=git --dry-run
-  assert_matches "v4.0.0" "$(git tag -l)"
+  assert_matches "v5.0.0" "$(git tag -l)"
 
   bash "$ROOT_DIR/release.sh" --plugins=git --quiet
-  assert_matches "v4.0.0" "$(git tag -l)"
+  assert_matches "v5.0.0" "$(git tag -l)"
 }
 test_commit_6_feat_scope_message() {
   git commit --quiet -m "feat(lang): add Polish language" --allow-empty
 
   bash "$ROOT_DIR/release.sh" --plugins=git --quiet --dry-run
-  assert_matches "v4.0.0" "$(git tag -l)"
+  assert_matches "v5.0.0" "$(git tag -l)"
 
   bash "$ROOT_DIR/release.sh" --plugins=git --quiet
-  assert_matches "v4.1.0" "$(git tag -l)"
+  assert_matches "v5.1.0" "$(git tag -l)"
 }
 test_commit_7_fix_multi_message() {
   git commit --quiet -m "fix: prevent racing of requests" -m "Introduce a request id and a reference to latest request. Dismiss incoming responses other than from latest request." -m "Remove timeouts which were used to mitigate the racing issue but are obsolete now" -m "Reviewed-by: Z" -m "Refs: #123" --allow-empty
 
   bash "$ROOT_DIR/release.sh" --plugins=git --quiet --dry-run
-  assert_matches "v4.1.0" "$(git tag -l)"
+  assert_matches "v5.1.0" "$(git tag -l)"
 
   bash "$ROOT_DIR/release.sh" --plugins=git --quiet
-  assert_matches "v4.1.1" "$(git tag -l)"
+  assert_matches "v5.1.1" "$(git tag -l)"
 }
 test_commit_8_revert_message() {
   git commit --quiet -m "revert: let us never again speak of the noodle incident" -m "Refs: 676104e, a215868" --allow-empty
 
   bash "$ROOT_DIR/release.sh" --plugins=git --quiet --dry-run --verbose
-  assert_matches "v4.1.1" "$(git tag -l)"
+  assert_matches "v5.1.1" "$(git tag -l)"
 
   bash "$ROOT_DIR/release.sh" --plugins=git
-  assert_matches "v4.1.2" "$(git tag -l)"
+  assert_matches "v5.1.2" "$(git tag -l)"
 }
--- tests/presets/workspace.nodejs.test.sh
+++ tests/presets/workspace.nodejs.test.sh
@@ -102,83 +102,95 @@ test_commit_0_3_skip_change() {
   bash "$ROOT_DIR/release.sh" --plugins=git --preset=workspace --workspace --quiet
   assert_matches "workspace1-v0.0.1" "$(git tag -l)"
 }
-test_commit_1_feat_breaking_major_message() {
-  git commit --quiet -m "feat(workspace1): allow provided config object to extend other configs" -m "BREAKING CHANGE: \`extends\` key in config file is now used for extending other config files" --allow-empty
+test_commit_1_0_stable_major_no_message() {
+  git commit --quiet -m "fix(workspace1): patch update" --allow-empty
 
   bash "$ROOT_DIR/release.sh" --plugins=git,npm,npm-post --preset=workspace --workspace --quiet --stable --quiet
   assert_matches "workspace1-v1.0.0" "$(git tag -l)"
+}
+test_commit_1_1_feat_breaking_major_message() {
+  git commit --quiet -m "feat(workspace1): allow provided config object to extend other configs" -m "BREAKING CHANGE: \`extends\` key in config file is now used for extending other config files" --allow-empty
+
+  bash "$ROOT_DIR/release.sh" --plugins=git,npm,npm-post --preset=workspace --workspace --quiet
+  assert_matches "workspace1-v2.0.0" "$(git tag -l)"
+}
+test_commit_1_1_feat_breaking_major_message_skip() {
+  git commit --quiet -m "feat(workspace2): allow provided config object to extend other configs" -m "BREAKING CHANGE: \`extends\` key in config file is now used for extending other config files" --allow-empty
+
+  bash "$ROOT_DIR/release.sh" --plugins=git --preset=workspace --workspace --quiet
+  assert_matches "workspace1-v2.0.0" "$(git tag -l)"
 }
 test_commit_2_feat_mark_major_message() {
   git commit --quiet -m "feat!: send an email to the customer when a product is shipped" --allow-empty
 
   bash "$ROOT_DIR/release.sh" --plugins=git,npm --preset=workspace --workspace --dry-run --quiet --quiet
-  assert_matches "workspace1-v1.0.0" "$(git tag -l)"
+  assert_matches "workspace1-v2.0.0" "$(git tag -l)"
 
   assert_matches "Your project has no new commits" "$(bash "$ROOT_DIR/release.sh" --plugins=git,npm,npm-post --preset=workspace --workspace --dry-run)"
 
   bash "$ROOT_DIR/release.sh" --plugins=git --preset=workspace --workspace --quiet
-  assert_matches "workspace1-v1.0.0" "$(git tag -l)"
+  assert_matches "workspace1-v2.0.0" "$(git tag -l)"
 }
 test_commit_3_feat_mark_scope_major_message() {
   git commit --quiet -m "feat(workspace1)!: send an email to the customer when a product is shipped" --allow-empty
 
   bash "$ROOT_DIR/release.sh" --plugins=git,npm,npm-post --preset=workspace --workspace --dry-run --quiet
-  assert_matches "workspace1-v1.0.0" "$(git tag -l)"
+  assert_matches "workspace1-v2.0.0" "$(git tag -l)"
 
   bash "$ROOT_DIR/release.sh" --plugins=git,npm,npm-post --preset=workspace --workspace --quiet
-  assert_matches "workspace1-v2.0.0" "$(git tag -l)"
+  assert_matches "workspace1-v3.0.0" "$(git tag -l)"
 }
 test_commit_4_feat_mark_breaking_scope_major_message() {
   git commit --quiet -m "chore(workspace1)!: drop support for Node 6" -m "BREAKING CHANGE: use JavaScript features not available in Node 6." --allow-empty
 
   bash "$ROOT_DIR/release.sh" --plugins=git,npm,npm-post --preset=workspace --workspace --dry-run --quiet
-  assert_matches "workspace1-v2.0.0" "$(git tag -l)"
+  assert_matches "workspace1-v3.0.0" "$(git tag -l)"
 
   bash "$ROOT_DIR/release.sh" --plugins=git,npm,npm-post --preset=workspace --workspace --quiet
-  assert_matches "workspace1-v3.0.0" "$(git tag -l)"
+  assert_matches "workspace1-v4.0.0" "$(git tag -l)"
 }
 test_commit_5_docs_root_no_update_message() {
   git commit --quiet -m "docs: correct spelling of CHANGELOG" --allow-empty
 
   bash "$ROOT_DIR/release.sh" --plugins=git,npm,npm-post --preset=workspace --workspace --dry-run --quiet
-  assert_matches "workspace1-v3.0.0" "$(git tag -l)"
+  assert_matches "workspace1-v4.0.0" "$(git tag -l)"
 
   bash "$ROOT_DIR/release.sh" --plugins=git,npm,npm-post --preset=workspace --workspace --quiet
-  assert_matches "workspace1-v3.0.0" "$(git tag -l)"
+  assert_matches "workspace1-v4.0.0" "$(git tag -l)"
 }
 test_commit_5_docs_workspace_no_update_message() {
   git commit --quiet -m "docs(workspace1): correct spelling of CHANGELOG" --allow-empty
 
   bash "$ROOT_DIR/release.sh" --plugins=git,npm,npm-post --preset=workspace --workspace --dry-run --quiet
-  assert_matches "workspace1-v3.0.0" "$(git tag -l)"
+  assert_matches "workspace1-v4.0.0" "$(git tag -l)"
 
   bash "$ROOT_DIR/release.sh" --plugins=git,npm,npm-post --preset=workspace --workspace --quiet
-  assert_matches "workspace1-v3.0.0" "$(git tag -l)"
+  assert_matches "workspace1-v4.0.0" "$(git tag -l)"
 }
 test_commit_6_feat_scope_message() {
   git commit --quiet -m "feat(workspace1): add Polish language" --allow-empty
 
   bash "$ROOT_DIR/release.sh" --plugins=git,npm,npm-post --preset=workspace --workspace --dry-run --quiet
-  assert_matches "workspace1-v3.0.0" "$(git tag -l)"
+  assert_matches "workspace1-v4.0.0" "$(git tag -l)"
 
   bash "$ROOT_DIR/release.sh" --plugins=git --preset=workspace --workspace --quiet
-  assert_matches "workspace1-v3.1.0" "$(git tag -l)"
+  assert_matches "workspace1-v4.1.0" "$(git tag -l)"
 }
 test_commit_7_fix_multi_message() {
   git commit --quiet -m "fix(workspace1): prevent racing of requests" -m "Introduce a request id and a reference to latest request. Dismiss incoming responses other than from latest request." -m "Remove timeouts which were used to mitigate the racing issue but are obsolete now" -m "Reviewed-by: Z" -m "Refs: #123" --allow-empty
 
   bash "$ROOT_DIR/release.sh" --plugins=git,npm,npm-post --preset=workspace --workspace --dry-run --quiet
-  assert_matches "workspace1-v3.1.0" "$(git tag -l)"
+  assert_matches "workspace1-v4.1.0" "$(git tag -l)"
 
   bash "$ROOT_DIR/release.sh" --plugins=git --preset=workspace --workspace --quiet
-  assert_matches "workspace1-v3.1.1" "$(git tag -l)"
+  assert_matches "workspace1-v4.1.1" "$(git tag -l)"
 }
 test_commit_8_revert_message() {
   git commit --quiet -m "revert(workspace1): let us never again speak of the noodle incident" -m "Refs: 676104e, a215868" --allow-empty
 
   bash "$ROOT_DIR/release.sh" --plugins=git,npm,npm-post --preset=workspace --workspace --dry-run --quiet
-  assert_matches "workspace1-v3.1.1" "$(git tag -l)"
+  assert_matches "workspace1-v4.1.1" "$(git tag -l)"
 
   bash "$ROOT_DIR/release.sh" --plugins=git,npm,npm-post --preset=workspace --workspace --quiet
-  assert_matches "workspace1-v3.1.2" "$(git tag -l)"
+  assert_matches "workspace1-v4.1.2" "$(git tag -l)"
 }
