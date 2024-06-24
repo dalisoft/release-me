dc8537c5bb172c68b85fd09debe79a3f48661740
Davlatjon Shavkatov <opensource@dalisoft.uz>
2024-06-24 22:46:37 +0500

    fix(plugins/npm-post): on GitHub it could be already saved

--- plugins/npm-post.sh
+++ plugins/npm-post.sh
@@ -34,12 +34,6 @@ prepare() {
     SSH_PUBLIC_KEY_FILE=$(mktemp)
     printf "%s" "${SSH_PUBLIC_KEY}" >>"${SSH_PUBLIC_KEY_FILE}"
 
-    git config --local commit.gpgsign true
-    git config --local user.signingkey "${SSH_PUBLIC_KEY_FILE}"
-    git config --local tag.forceSignAnnotated true
-    git config --local gpg.format ssh
-    log_verbose "Git SSH sign key is set"
-
     if [ -n "${SSH_KEY_PASSPHRASE-}" ]; then
       SSH_ASKPASS_FILE=$(mktemp)
       chmod 0755 "${SSH_ASKPASS_FILE}"
@@ -50,12 +44,14 @@ prepare() {
       log_verbose "Git SSH passphrase set"
     fi
 
-    if ! ssh-add -L | grep -q "${SSH_PUBLIC_KEY-}"; then
-      printf "%s" "${SSH_PRIVATE_KEY}" | base64 --decode | ssh-add -
-      log_verbose "Git SSH key import loaded"
-    else
-      log_verbose "Git SSH key import skipped"
-    fi
+    printf "%s" "${SSH_PRIVATE_KEY}" | base64 --decode | ssh-add -
+    log_verbose "Git SSH key import loaded"
+
+    git config --local commit.gpgsign true
+    git config --local user.signingkey "${SSH_PUBLIC_KEY_FILE}"
+    git config --local tag.forceSignAnnotated true
+    git config --local gpg.format ssh
+    log_verbose "Git SSH sign key is set"
 
     if [ -n "${SSH_KEY_PASSPHRASE-}" ]; then
       rm -rf "${SSH_ASKPASS}"
